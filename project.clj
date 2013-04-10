1899897bc11356c802ecbcc357b1adc994fb8296
Aaron Bernard <aaron.bernard@duramec.com>
2013-04-10 06:09:37 -0500

    fixed paths for lein javac compile

--- project.clj
+++ project.clj
@@ -2,4 +2,8 @@
             :description "Duramec id management library"
             :repositories [["internal" {:url "https://repo.duramec.com/internal"
                                         :creds :gpg}]]
+            :java-source-paths ["src/java"]
+            :javac-options["-target" "1.7"
+                           "-source" "1.7"
+                           "-deprecation"]
             :dependencies [[com.duramec/time "0.1.3"]])
